3d397b9bfbd9485aecd901a30df1321291e3ecc5
gagatpadi <padiyogaradhana@gmail.com>
2024-03-26 17:34:03 +0700

    Add project files.

---
+++ FungsiSwitch/FungsiSwitch.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int bilA, bilB;
+
+void input()
+{
+	cout << "Masukan Bilangan Pertama = ";
+	cin >> bilA;
+	cout << "Masukan bilangan kedua = ";
+	cin >> bilB;
+}
+
+int penjumlahan(int a, int b)
+{
+	return a + b;
+
+}
